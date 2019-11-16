8a463006cde26488685e076c4356cf45d004c308
Shivank <shivankkapoor5@gmail.com>
2019-11-16 13:05:04 +0530

    Third Patch

--- home.py
+++ home.py
@@ -9,6 +9,15 @@ def home():
 @app.route("/SignIn")
 def signin():
     return render_template("signin.html")
+
+@app.route("/Register")
+def register():
+    return render_template("register.html")
+
+@app.route("/P1info")
+def p1():
+    return render_template("p1info.html")
+
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
