e946a12e2e1eade01f1e196daa3875cce96483df
Shivank <shivankkapoor5@gmail.com>
2019-11-17 14:40:25 +0530

    Connections Patch-3

--- firebaseTest.py
+++ firebaseTest.py
@@ -12,45 +12,15 @@ config = {
     "messagingSenderId": "4547496317",
     "appId": "1:4547496317:web:8f3caa80b97db683e6a414",
     "measurementId": "G-GGYG3JXW08"
-  }
+          }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-    
-@app.route("/SignIn",methods=["GET", "POST"])
-def signin():
-    if request.method == "POST":
-        
-      username = request.form["username"]   #Data Extracted from HTML
-      password = request.form["pass"]
-      try:
-          auth.sign_in_with_email_and_password(email, password)      	
-      except:
-	  return 'check your credentials
-      return render_template("idea.html")
-    else:
-      return render_template("signin.html")
-    
+email = input('please enter email \n')
+password = input('password \n')
 
-@app.route("/Register")
-def register():
-    return render_template("register.html")
-
-@app.route("/P1info")
-def p1():
-    return render_template("p1info.html")
-
-if __name__ == "__main__":
-    app.run()
-
-#email = input('please enter email \n')
-#password = input('password \n')
-
-#user = auth.create_user_with_email_and_password(email, password)
+user = auth.create_user_with_email_and_password(email, password)
 #auth.send_email_verification(user['idToken'])
 #auth.send_password_reset_email(email)
 #print(auth.get_account_Info(user['idToken']))
--- home.py
+++ home.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template,request,flash,session,redirect  
-#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-#app.config["MONGO_URI"] = ""
-#mongo =PyMongo(app)
 
 @app.route("/")
 def home():
@@ -19,13 +16,33 @@ def signin():
       return render_template("signin.html")
     
 
-@app.route("/Register")
+@app.route("/Register", methods=["GET", "POST"])
 def register():
-    return render_template("register1.html")
+     if request.method == "POST":
+      useremail = request.form["email"]   #Data Extracted from HTML
+      password = request.form["password"]
+      cpassword = request.form["confirming"]
+      if (password != cpassword):
+         return render_template("register1.html")
+      return render_template("idea.html")     
+     else:
+        return render_template("register1.html")
+
+@app.route("/Idea")
+def idea():
+    return render_template("idea.html")
 
 @app.route("/P1info")
 def p1():
     return render_template("p1info.html")
+
+@app.route("/P2info")
+def p2():
+    return render_template("p2info.html")
+
+@app.route("/P3info")
+def p3():
+    return render_template("p3info.html")
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
