7a9dfb604a3fdfc2fb2bb9616b3e3b7da44c19b6
Shivank <shivankkapoor5@gmail.com>
2019-11-17 17:57:53 +0530

    FireBase Connection Patch-1

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template,request,flash,session,redirect
+import pyrebase
 
 app = Flask(__name__)
+
+config = {
+    "apiKey": "AIzaSyAdquXgxR_M9NsQpW50vsz0v6WwvvBC17A",
+    "authDomain": "dellhackathon-e28e7.firebaseapp.com",
+    "databaseURL": "https://dellhackathon-e28e7.firebaseio.com",
+    "projectId": "dellhackathon-e28e7",
+    "storageBucket": "dellhackathon-e28e7.appspot.com",
+    "messagingSenderId": "4547496317",
+    "appId": "1:4547496317:web:8f3caa80b97db683e6a414",
+    "measurementId": "G-GGYG3JXW08"
+          }
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 @app.route("/")
 def home():
@@ -23,6 +38,8 @@ def register():
       cpassword = request.form["confirming"]
       if (password != cpassword):
          return render_template("register1.html")
+      else:
+         user = auth.create_user_with_email_and_password(useremail, password)  #User created in the Firebase Database
       return render_template("idea.html")     
      else:
         return render_template("register1.html")
