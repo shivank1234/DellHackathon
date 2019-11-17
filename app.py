bc1c09235b873f10fcc8535534e04ee46d9b3343
ben10nyson <39547625+ben10nyson@users.noreply.github.com>
2019-11-17 14:03:26 +0530

    changed css
    changed css so that homepage can use it, changed a little bit of home.py(now renamed to app.py for flask) and tried to link the buttons to go to another page. Still making changes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,flash,session,redirect  
+from flask import Flask, render_template,request,flash,session,redirect
 #from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-    
-@app.route("/SignIn",methods=["GET", "POST"])
+
+@app.route("/signin.html",methods=["GET", "POST"])
 def signin():
     if request.method == "POST":
       username = request.form["username"]   #Data Extracted from HTML
@@ -17,15 +17,15 @@ def signin():
       return render_template("idea.html")
     else:
       return render_template("signin.html")
-    
 
-@app.route("/Register")
+
+@app.route("/register1.html")
 def register():
     return render_template("register1.html")
 
 @app.route("/P1info")
 def p1():
     return render_template("p1info.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
